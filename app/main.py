335785c9d75557f7a263c6113c4a1fb42eb2c872
Nazar <nazargorodyko@gmail.com>
2024-09-09 20:50:41 +0300

    Fixed errors

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list[Car]) -> float:
         cars_sum = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -23,8 +23,7 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(cars_sum, 1)
 
-    def calculate_washing_price(self, car: Car) \
-            -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         washing_price = (car.comfort_class
                          * (self.clean_power - car.clean_mark)
                          * self.average_rating
